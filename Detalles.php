923aadfed180db6c0746b9305b1bcd2e38fbd740
Vicky <crocevictoria@gmail.com>
2024-09-11 22:10:08 -0300

    commit

--- Detalles.php
+++ Detalles.php
@@ -1,2 +1,17 @@
 <?php
+include("BaseDeDatos/Conexion.php");
 
+/*$conexion = conectar();
+echo "se realizo un conexion con la base de datos";*/
+
+$pokemon = fetch();
+
+if ($pokemon) {
+    echo $pokemon['nombre'];
+    echo $pokemon['imagen'];
+    echo "Tipo: " . $pokemon['tipo'] . "<br>";
+    echo "Descripción:". $pokemon['descripcion'];
+} else {
+    echo "<p>Pokémon no encontrado.</p>";
+}
+?>
