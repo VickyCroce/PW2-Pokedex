86bde918ae97c0743c6c57ade280b56f323b3486
Milagros Pessino <milipessi@gmail.com>
2024-09-24 11:52:18 -0300

    separar estilos

--- agregar.php
+++ agregar.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($_GET['id']) ? 'Editar Pokémon' : 'Agregar Pokémon'; ?></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="estilos/style.css">
+    <link rel="stylesheet" href="estilos/agregarPokemon.css">
     <link rel="icon" href="img/pokebolaLogo.png" type="image/png">
 </head>
 <body>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokedex Modificada</title>
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="estilos/header.css">
     <link rel="icon" href="img/pokebolaLogo.png" type="image/png">
 </head>
 <body>
