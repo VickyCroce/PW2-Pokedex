a5b7b9cc0543b597868a3615be370eca6bc2af1c
Milagros Pessino <milipessi@gmail.com>
2024-09-30 19:06:42 -0300

    listo

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pokedex Modificada</title>
+    <title>Pokedex</title>
     <link rel="icon" href="img/pokebolaLogo.png" type="image/png">
     <link rel="stylesheet" href="estilos/header.css">
 </head>
